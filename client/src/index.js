b38c42ec038d06a417f90287927a90c1b377f7c0
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2024-06-18 18:24:14 +0530

    production ready

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+if (process.env.TYPE_OFF_APP === 'production') disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
-    </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
